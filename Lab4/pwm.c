681ba192a54e081e183b52f2df9b37fde5a93b80
JimPatrizi <jdp99@case.edu>
2017-11-02 18:07:50 -0400

    ha we had the led the wrong way, our code was code for pwm.c

--- Lab4/pwm.c
+++ Lab4/pwm.c
@@ -1,18 +1,58 @@
+/*
+ * pwm.c:
+ *	This tests the hardware PWM channel.
+ *
+ * Copyright (c) 2012-2013 Gordon Henderson. <projects@drogon.net>
+ ***********************************************************************
+ * This file is part of wiringPi:
+ *	https://projects.drogon.net/raspberry-pi/wiringpi/
+ *
+ *    wiringPi is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU Lesser General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    wiringPi is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public License
+ *    along with wiringPi.  If not, see <http://www.gnu.org/licenses/>.
+ ***********************************************************************
+ */
 
 #include <wiringPi.h>
-#include <softPwm.h>
 
-//cc -o myprog myprog.c -lwiringPi -lpthread
+#include <stdio.h>
 
-int main()
+
+int main (void)
 {
-	if (wiringPiSetup () == -1) //using wPi pin numbering
-		exit (1) ;
+  int bright ;
 
-	pinMode(1, PWM_OUTPUT);
-	pwmSetMode(PWM_MODE_MS); 
-	pwmSetClock(384); //clock at 50kHz (20us tick)
-	pwmSetRange(1000); //range at 1000 ticks (20ms)
-	pwmWrite(1, 75);  //theretically 50 (1ms) to 100 (2ms) on my servo 30-130 works ok
-	return 0 ;
+  printf ("Raspberry Pi wiringPi PWM test program\n") ;
+
+  if (wiringPiSetup() == -1)
+		return;
+
+  pinMode (1, PWM_OUTPUT) ;
+while(1)
+{
+    for (bright = 0 ; bright < 1024 ; bright++)
+    {
+      pwmWrite (1, bright) ;
+      delay (1) ;
+    }
+    delay(1000);
+
+    for (bright = 1023 ; bright >= 0 ; bright--)
+    {
+      pwmWrite (1, bright) ;
+      delay (1) ;
+    }
+}
+  
+
+  return 0 ;
 }
